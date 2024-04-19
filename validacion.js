cf5402829616f79d37ea5206cc0894b27e27c9dc
AbrahamOliden <abrahamoliden@protonmail.com>
2024-04-18 23:57:50 -0600

    Event listener for input button

--- validacion.js
+++ validacion.js
@@ -1,1 +1,20 @@
-//Haz tú validación en javascript acá
+//Variables
+
+const inputButton = document.getElementById('button-input');
+
+// Event listeners
+
+inputButton.addEventListener('click', validateFormInput.bind(null));
+
+var nameInputValue = document.getElementById('name-input').value;
+var emailInputValue = document.getElementById('email-input').value;
+var subjectInputValue = document.getElementById('subject-input').value;
+var messageInputValue = document.getElementById('message-input').value;
+
+function validateFormInput() {
+    alert('Aqui andamos gfe');
+};
+
+function validateNameField() {
+
+}
