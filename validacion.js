0cb38b209159e436f7e577247d777c18b329617c
AbrahamOliden <abrahamoliden@protonmail.com>
2024-04-20 19:44:20 -0600

    merged all functions' into one validateInput() function

--- validacion.js
+++ validacion.js
@@ -1,81 +1,31 @@
 //Variables
-const nonEmptyRegex = /\S/g;
-const inputButton = document.getElementById('button-input');
+
+const nameInput = document.getElementById('name-input');
+const emailInput = document.getElementById('email-input');
+const subjectInput = document.getElementById('subject-input');
+const messageInput = document.getElementById('message-input');
 
 // Event listeners
 
-inputButton.addEventListener('click', validateFormInput.bind(null));
+nameInput.addEventListener('focusout', validateInput.bind(element));
+emailInput.addEventListener('focusout', validateInput.bind(element));
+subjectInput.addEventListener('focusout', validateInput.bind(element));
+messageInput.addEventListener('focusout', validateInput.bind(element));
 
-function validateFormInput() {
-    // Variables
-    const nameInputValue = document.getElementById('name-input').value;
-    const emailInputValue = document.getElementById('email-input').value;
-    const subjectInputValue = document.getElementById('subject-input').value;
-    const messageInputValue = document.getElementById('message-input').value;
+function validateInput() {
+    //Variables
+    const inputValue = this.value;
 
-    validateNameField(nameInputValue)
-        ? alert('name')
-        : alert('no name');
-    validateEmailField(emailInputValue)
-        ? alert('email')
-        : alert('no email');
-    validateSubjectField(subjectInputValue)
-        ? alert('subject')
-        : alert('no subject');
-    validateMessageField(messageInputValue)
-        ? alert('message')
-        : alert('no message');
-};
-
-function validateNameField(nameInput) {
-
-    if ( !nonEmptyRegex.test(nameInput) ) {
-        alert("nombre ta basio");
-        return false;
-    } else if (nameInput.length > 50) {
-        alert("Too mich name");
-        return false;
-    };
-
-    return true;
-};
-
-function validateEmailField(emailInput) {
+    const nonEmptyRegex = /\S/g;
     const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    if ( !nonEmptyRegex.test(emailInput) ) {
-        alert("email ta basio");
-        return false;
-    } else if ( !emailRegex.test(emailInput) ) {
-        alert('Please enter a valid email address');
-        return false;
-    };
+    if ( !nonEmptyRegex(inputValue) ) return false;
 
-    return true;
+    if (this.id === 'message-input') {
+        return inputValue.length <= 300;
+    } else if (this.id === 'email-input') {
+        return emailRegex.test(inputValue);
+    } else {
+        return inputValue.length <= 50;
+    };
 };
-
-function validateSubjectField(subjectInput) {
-
-    if ( !nonEmptyRegex.test(subjectInput) ) {
-        alert('subject ta basio');
-        return false;
-    } else if (subjectInput.length > 50) {
-        alert('Too much subject');
-        return false;
-    };
-
-    return true;
-};
-
-function validateMessageField(messageInput) {
-
-    if( !nonEmptyRegex.test(messageInput) ) {
-        alert('message to basio');
-        return false;
-    } else if (messageInput.length > 300) {
-        alert('Too much message');
-        return false;
-    };
-
-    return true;
-}
